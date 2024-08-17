9f9e77d4bd9beeea5e95ed74a8d4df7253ec3089
Youssef ElMountahi <yusuf.one@outlook.com>
2024-08-17 23:53:08 +0100

    starting heredoc norm

--- heredoc/ft_add_heredoc.c
+++ heredoc/ft_add_heredoc.c
@@ -6,43 +6,49 @@
 /*   By: yel-moun <yel-moun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 12:29:45 by yel-moun          #+#    #+#             */
-/*   Updated: 2024/08/15 13:11:12 by yel-moun         ###   ########.fr       */
+/*   Updated: 2024/08/17 23:21:18 by yel-moun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_heredoc *ft_add_heredoc(t_heredoc **heredoc_list, t_tokens_list *tokens)
+void	process_tokens(t_tokens_list *tokens,
+	t_heredoc **heredoc_list, int *index, int *list_size)
 {
-	t_heredoc		*heredoc;
 	t_tokens_list	*tmp;
-	int				index;
-	int				list_size;
+	t_heredoc		*heredoc;
 
-	if (!heredoc_list || !tokens)
-		return (NULL);
 	tmp = tokens;
-	index = 0;
-	list_size = 0;
 	while (tmp)
 	{
 		if (tmp->type == PIPE)
 		{
-			index ++;
+			(*index)++;
 			tmp = tmp->next;
 			continue ;
 		}
 		if (tmp->type == HEREDOC)
 		{
-			list_size ++;
-		heredoc = ft_create_heredoc(&tmp, list_size);
-		if (heredoc)
-			heredoc->index = index;
-		ft_heredoc_addback(heredoc_list, heredoc);	
+			(*list_size)++;
+			heredoc = ft_create_heredoc(&tmp, *list_size);
+			if (heredoc)
+				heredoc->index = *index;
+			ft_heredoc_addback(heredoc_list, heredoc);
 		}
 		if (tmp)
 			tmp = tmp->next;
 	}
-	return (*heredoc_list);
 }
 
+t_heredoc	*ft_add_heredoc(t_heredoc **heredoc_list, t_tokens_list *tokens)
+{
+	int				index;
+	int				list_size;
+
+	if (!heredoc_list || !tokens)
+		return (NULL);
+	index = 0;
+	list_size = 0;
+	process_tokens(tokens, heredoc_list, &index, &list_size);
+	return (*heredoc_list);
+}
